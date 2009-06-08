0028a849b4543f1cb6e2bb8ff1487380f6b017c8
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-06-08 18:11:39 +0000

    Added flowFP package to svn
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowFP@40007 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/unitTests/runit.flowFPPlex.R
@@ -0,0 +1,56 @@
+# Simple test.
+#
+#  create a fingerprint plex and make sure it works.
+test.plex1 <- function() {
+	data(fs1)
+	fp <- flowFP(fs1)
+	plex = flowFPPlex(c(fp, fp))
+	checkTrue(!is.null(plex))
+	checkTrue(is(plex) == "flowFPPlex")
+}
+
+# More advanced test.
+#
+#  create a fingerprint plex and make sure it works.
+test.plex2 <- function() {
+	data(fs1, fs2)
+	fp <- flowFP(fs1)
+	plex = flowFPPlex(c(fp, fp))
+	checkTrue(!is.null(plex))
+	checkTrue(is(plex) == "flowFPPlex")
+}
+
+
+test.plex_append <- function() {
+	data(fs1, fs2)
+	fp <- flowFP(fs1)
+	plex = flowFPPlex()
+	checkTrue(!is.null(plex))
+	checkTrue(is(plex) == "flowFPPlex")
+	plex = append(plex, fp)
+	checkTrue(is.flowFPPlex(plex))
+}
+
+
+test.plex_testClasses <- function() {
+	data(fs1, fs2)
+	fp1 <- flowFP(fs1,sampleClasses=c(rep("A", 3), rep("B", 4)))
+	fp2 <- flowFP(fs2,sampleClasses=c(rep("A", 3), rep("B", 4)))
+	plex = flowFPPlex(c(fp1, fp2))
+	checkTrue(!is.null(plex))
+	checkTrue(is.flowFPPlex(plex))
+}
+
+test.plex_append_plex <- function() {
+	data(fs1, fs2)
+	fp <- flowFP(fs1)
+	plex = flowFPPlex()
+	checkTrue(!is.null(plex))
+	checkTrue(is(plex) == "flowFPPlex")
+	plex = append(plex, fp)
+	checkTrue(is.flowFPPlex(plex))
+	plex2 = append(plex, c(fp, fp))
+	checkTrue(is.flowFPPlex(plex2))
+	plex3 = append(plex, plex2)
+	checkTrue(is.flowFPPlex(plex3))
+}
